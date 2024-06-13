44dd01b269c115ba8b973c536bae84cbd727adb3
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-13 23:48:45 +0200

    Add empty ReusableListTile

---
+++ lib/reusable/list_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReusableListTile extends StatelessWidget {
+  const ReusableListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
