5d773f8e0cb0d4087e699067cf1bfbf90eb654a9
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-13 23:52:46 +0200

    add first required variable and use it in ListTitle widget

--- lib/reusable/list_tile.dart
+++ lib/reusable/list_tile.dart
@@ -1,10 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class ReusableListTile extends StatelessWidget {
-  const ReusableListTile({super.key});
+  final String title;
+  const ReusableListTile({
+    super.key,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListTile(
+      title: Text(title),
+    );
   }
 }
