cfbdcf4472ae4956e010a3defdaf2b49b1b1f8cd
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-13 23:57:46 +0200

    Add to listView two reusable list tile to show how it looks in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo_app/reusable/list_tile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,6 +10,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> titles = ['first title', 'seccond title'];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -21,8 +23,15 @@ class MainApp extends StatelessWidget {
                 color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ),
-        body: const Center(
-          child: Text('Hello World!'),
+        body: ListView(
+          children: [
+            ReusableListTile(
+              title: titles[0],
+            ),
+            ReusableListTile(
+              title: titles[1],
+            )
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.purple,
