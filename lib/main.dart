75277b4611cfa28c4165dad95e799ba71d0bcf53
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-14 22:25:01 +0200

    add mocked numbers values to ReusableListTile
    Add Scaffold background color
    
    Create List of Maps to use it to ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> titles = ['first title', 'seccond title'];
+    List<String> titles = ['first title', 'seccond titile'];
+    List<Map<String, dynamic>> mapTiles = [
+      {
+        'number': '1',
+        'title': 'title one',
+        'subtitle': 'subtitle one',
+      },
+      {
+        'number': '2',
+        'title': 'title two',
+        'subtitle': 'subtitle two',
+      },
+      {
+        'number': '3',
+        'title': 'title three',
+        'subtitle': 'subtitle three',
+      }
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.grey[300],
         appBar: AppBar(
           backgroundColor: Colors.purple,
           centerTitle: true,
           title: const Text(
             'What we need to do?',
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold),
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ),
         body: ListView(
           children: [
             ReusableListTile(
+              number: '',
               title: titles[0],
             ),
             ReusableListTile(
+              number: '',
               title: titles[1],
             )
           ],
