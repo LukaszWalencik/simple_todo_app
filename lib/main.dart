2047d0f6889d1019f62ce8404521d8bfbedd70e5
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-14 23:50:01 +0200

    Replace whole Scaffold with HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_todo_app/reusable/list_tile.dart';
+import 'package:simple_todo_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,54 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> mapTiles = [
-      {
-        'number': '1',
-        'title': 'title one',
-        'subtitle': 'subtitle one',
-      },
-      {
-        'number': '2',
-        'title': 'title two',
-        'subtitle': 'subtitle two',
-      },
-      {
-        'number': '3',
-        'title': 'title three',
-        'subtitle': 'subtitle three',
-      }
-    ];
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          centerTitle: true,
-          title: const Text(
-            'What we need to do?',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: ListView.builder(
-          itemCount: mapTiles.length,
-          itemBuilder: (context, index) {
-            return ReusableListTile(
-              number: mapTiles[index]['number'],
-              title: mapTiles[index]['title'],
-              subtitle: mapTiles[index]['subtitle'],
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.purple,
-          onPressed: () {},
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
