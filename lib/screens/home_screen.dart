e52609732c413fffea5751d704e648b89b34ce95
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-14 23:49:22 +0200

    Cut whole Scaffold widget with body and paste it as body of HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo_app/reusable/list_tile.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Map<String, dynamic>> mapTiles = [
+      {
+        'number': '1',
+        'title': 'title one',
+        'subtitle': 'subtitle one',
+      },
+      {
+        'number': '2',
+        'title': 'title two',
+        'subtitle': 'subtitle two',
+      },
+      {
+        'number': '3',
+        'title': 'title three',
+        'subtitle': 'subtitle three',
+      }
+    ];
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        title: const Text(
+          'What we need to do?',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: mapTiles.length,
+        itemBuilder: (context, index) {
+          return ReusableListTile(
+            number: mapTiles[index]['number'],
+            title: mapTiles[index]['title'],
+            subtitle: mapTiles[index]['subtitle'],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
