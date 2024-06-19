af0fec0374ac9c8316dc9cad43c3feb8c491de4b
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-19 21:42:49 +0200

    Add WidgetFlutterBinding and inject SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_todo_app/global/global.dart';
 import 'package:simple_todo_app/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
   runApp(const MainApp());
 }
 
