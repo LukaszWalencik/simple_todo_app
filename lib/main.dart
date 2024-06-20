2804b08f1f98077068c913ac99c294b5204fd87f
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-20 16:17:31 +0200

    initial Firebase in prioject and use Options for current platform what user run

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_todo_app/firebase_options.dart';
 import 'package:simple_todo_app/global/global.dart';
 import 'package:simple_todo_app/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   sharedPreferences = await SharedPreferences.getInstance();
   runApp(const MainApp());
 }
