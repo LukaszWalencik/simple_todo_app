281a4624c490a430459495df8910420c2ab29d4a
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-21 21:53:19 +0200

    Add Colum as body and central Text on screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_todo_app/reusable/text_field.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -11,15 +12,27 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          centerTitle: true,
-          title: const Text(
-            'Simple TodoApp',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        title: const Text(
+          'Simple TodoApp',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        body: Container());
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+          ),
+          const Text(
+            'Login to your account',
+            style: TextStyle(
+                color: Colors.black, fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
