6eccd957a3867e1fc50ef6895325c4694125f7e3
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-06-26 22:07:34 +0200

    Add horizontal padding

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -26,77 +26,80 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.2,
-            ),
-            const Text(
-              'Login to your account',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            ReusableTextField(
-                textInputAction: TextInputAction.next,
-                title: 'Login',
-                icon: const Icon(Icons.person),
-                textEditingController: loginController),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.02,
-            ),
-            ReusableTextField(
-                textInputAction: TextInputAction.done,
-                title: 'Password',
-                icon: const Icon(Icons.lock),
-                textEditingController: passwordController),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => RegisterScreen()));
-              },
-              child: const Text(
-                'New use? Click here!',
-                style: TextStyle(color: Colors.purple, fontSize: 16),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
               ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  color: Colors.purple,
-                ),
+              const Text(
+                'Login to your account',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
                 height: MediaQuery.of(context).size.height * 0.05,
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.login_outlined,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
+              ),
+              ReusableTextField(
+                  textInputAction: TextInputAction.next,
+                  title: 'Login',
+                  icon: const Icon(Icons.person),
+                  textEditingController: loginController),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              ReusableTextField(
+                  textInputAction: TextInputAction.done,
+                  title: 'Password',
+                  icon: const Icon(Icons.lock),
+                  textEditingController: passwordController),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const RegisterScreen()));
+                },
+                child: const Text(
+                  'New use? Click here!',
+                  style: TextStyle(color: Colors.purple, fontSize: 16),
                 ),
               ),
-            )
-          ],
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.purple,
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.login_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
